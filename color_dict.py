9293133da3037fa5b1bbfc95608c420e99c68d61
Lori Pfahler <lpfahler@comcast.net>
2024-02-01 20:19:43 -0500

    Lesson 54

---
+++ color_dict.py
@@ -0,0 +1,21 @@
+# Create a color dictionary that I can import into any
+# micropython program
+# Lori Pfahler
+# January 2024
+
+colors = {
+    'RED'              : (255, 0, 0),
+    'ORANGE'           : (255, 127, 0),
+    'YELLOW'           : (255, 255, 0),
+    'SPRING GREEN'     : (127, 255, 0),
+    'GREEN'            : (0, 255, 0),
+    'TURQUOISE'        : (0, 255, 127),
+    'CYAN'             : (0, 255, 255),
+    'OCEAN'            : (0, 127, 255),
+    'BLUE'             : (0, 0, 255),
+    'VIOLET'           : (127, 0, 255),
+    'MAGENTA'          : (255, 0, 255),
+    'RASPBERRY'        : (255, 0, 127),
+    'WHITE'            : (255, 255, 255),
+    'BLACK'            : (0, 0, 0),   
+    }
